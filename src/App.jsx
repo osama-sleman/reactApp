6bfbc63092f16d1ff966454f8f2ead1ec624c54d
Osama Sleiman <117354814+osama-sleman@users.noreply.github.com>
2024-04-28 19:23:54 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Main from './components/Main'
+
+function App() {
+
+  return (
+    
+      <div className='container'>
+       <Main />
+      </div>
+
+  )
+}
+
+export default App
---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import reactLogo from './assets/react.svg'
+
+export default function Main(){
+    return(
+    <div>
+        <h1>hello</h1>
+    </div>
+    )
+}
